2d77d413af0a6f09aea1124d613662bc2e68a8eb
i'DLisT <i.dlist@outlook.com>
2022-05-27 23:21:21 +0800

    test: index

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,6 +1,6 @@
 import test from 'ava'
 import r from '../src/index'
-import { EachCharRegExp } from './utils'
+import { EachCharRegExp, ColorRegExp } from './utils'
 
 test('Render color directly', t => {
   t.is(r.white('text'), '\x1b[38;2;255;255;255mtext\x1b[0m')
@@ -20,4 +20,24 @@ test('Generate context from color invoke', t => {
 
 test('Render rainbow color directly', t => {
   t.regex(r.rainbow('text'), EachCharRegExp)
+})
+
+test('Generate context from modifier: bg', t => {
+  t.is(r.bg.white('text'), '\x1b[48;2;255;255;255mtext\x1b[0m')
+})
+
+test('Generate context from modifier: dark', t => {
+  t.regex(r.dark.red('text'), ColorRegExp)
+})
+
+test('Generate context from modifier: light', t => {
+  t.regex(r.light.red('text'), ColorRegExp)
+})
+
+test('Generate context from method: rgb', t => {
+  t.is(r.rgb(255, 255, 255)('text'), '\x1b[38;2;255;255;255mtext\x1b[0m')
+})
+
+test('Generate context from method: hsv', t => {
+  t.is(r.hsv(0, 100, 100)('text'), '\x1b[38;2;255;0;0mtext\x1b[0m')
 })
